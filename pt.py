25eec153291e5c00fc62f6d27dbbddfdd6980965
Nikolai Petrov <Petroffn@users.noreply.github.com>
2021-08-11 14:16:19 +0300

    Create pt.py

---
+++ pt.py
@@ -0,0 +1,11 @@
+# 1000000 <- не считайте это# 1000000 <-- do not count this
+a = 123.4
+b = -34.21
+# I've removed dot before 45
+c = 56.7100044445
+d = -555.72
+e = +234.2
+f = str('false')
+g = str('False 2v')
+
+print(a+b+c+d+e)
